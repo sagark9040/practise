bf19aaeb7300c1494c00b6fb9ef9c8982e82275c
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-05 21:50:11 -0400

    Added recursive solution to check palindrome LL

--- src/ctci/Chp2_linkedLists/Q26_LinkedListPalindrome.java
+++ src/ctci/Chp2_linkedLists/Q26_LinkedListPalindrome.java
@@ -8,14 +8,36 @@ import java.util.Stack;
 
 public class Q26_LinkedListPalindrome {
 
-	public static void extraSpaceIsPalindrome(Node head){
+	static PalinResult recurPalin(Node head, int length){
+		if(head == null) return new PalinResult(head, true);
+		
+		if(length <=0) return new PalinResult(head, true);
+			
+		if(length == 1) return new PalinResult(head.getNext(), true);
+		
+		PalinResult result = recurPalin(head.getNext(), length-2);
+		
+		if(result.result == false)
+			return result;
+		
+		if(result.node.getData() != head.getData())
+			return new PalinResult(head, false);
+		
+		result.node = result.node.getNext();
+		
+		return result;
+		
+		
+	}
+	
+	static void extraSpaceIsPalindrome(Node head){
 		Stack<Integer> stack = new Stack<Integer>();
 		int length = 0;
 		Node curNode = head;
 		
 		while(curNode != null){
-			stack.push(curNode.data);
-			curNode = curNode.next;
+			stack.push(curNode.getData());
+			curNode = curNode.getNext();
 			length++;
 		}
 		
@@ -25,15 +47,16 @@ public class Q26_LinkedListPalindrome {
 		
 		while(counter < length/2){
 			int curStackElem = stack.pop();
-			if(curStackElem != curNode.data){
+			if(curStackElem != curNode.getData()){
 				System.out.println("Not a palindrome");
 				isPalindrome = false;
 				return;
 			}
-			curNode = curNode.next;
+			curNode = curNode.getNext();
 			counter++;
 		}
-				
+		
+		
 		if(isPalindrome)
 			System.out.println("Is Palindrome");
 	}
@@ -46,13 +69,15 @@ public class Q26_LinkedListPalindrome {
 	public static void isPalindrome(){
 		LinkedList first = new LinkedList();
 
+		first.addNode(new Node(1));
 		first.addNode(new Node(7));
 		first.addNode(new Node(9));
 		first.addNode(new Node(6));
 		first.addNode(new Node(9));
 		first.addNode(new Node(7));
+		first.addNode(new Node(1));
 		
-		extraSpaceIsPalindrome(first.head());
+		System.out.println(recurPalin(first.head(), 5).result);
 	}
 	
 	public static void notPalindrome(){
@@ -63,7 +88,17 @@ public class Q26_LinkedListPalindrome {
 		first.addNode(new Node(6));
 		first.addNode(new Node(9));
 		
-		extraSpaceIsPalindrome(first.head());
+		System.out.println(recurPalin(first.head(), 4).result);
 	}
 	
+}
+
+class PalinResult{
+	Node node;
+	boolean result;
+	
+	PalinResult(Node node, boolean result){
+		this.node = node;
+		this.result = result;
+	}
 }
