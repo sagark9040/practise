72120c7d1558c456a987ac3115e7b8ba73f5689a
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-06 23:56:05 -0400

    Graph base class added for this chapter

---
+++ src/ctci/Chp4_TreesAndGraphs/Graph.java
@@ -0,0 +1,47 @@
+package ctci.Chp4_TreesAndGraphs;
+
+public class Graph {
+	GraphNode[] graphNodes;
+	public int count;
+
+	public GraphNode[] getNodes() {
+		return graphNodes;
+	}	
+	public void addNode(GraphNode x) {
+		if (count < graphNodes.length) {
+			graphNodes[count] = x;
+			count++;
+		} else {
+			System.out.print("Graph full");
+		}
+    }
+	
+}
+
+class GraphNode{
+	String data;
+	GraphNode[] adjacentNodes;
+	State state;
+	public int adjacentCount;
+	
+	public GraphNode(String vertex, int adjacentLength) {
+        this.data = vertex;                
+        adjacentCount = 0;        
+        adjacentNodes = new GraphNode[adjacentLength];
+    }
+	
+	public GraphNode[] getAdjacentNodes() {
+		return adjacentNodes;
+	}
+
+	public void addAdjacent(GraphNode graphNode) {
+		if (adjacentCount < adjacentNodes.length) {
+            this.adjacentNodes[adjacentCount] = graphNode;
+            adjacentCount++;
+        } else {
+            System.out.print("No more adjacent can be added");
+        }
+    }
+}
+
+enum State {Visited, NotVisited;}
