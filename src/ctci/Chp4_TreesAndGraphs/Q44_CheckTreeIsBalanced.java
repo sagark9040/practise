9dccb3866b38dca9f2b15437bce3acbad17d97fc
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-08 16:03:03 -0400

    Check if tree is balanced

--- src/ctci/Chp4_TreesAndGraphs/Q44_CheckTreeIsBalanced.java
+++ src/ctci/Chp4_TreesAndGraphs/Q44_CheckTreeIsBalanced.java
@@ -60,32 +60,53 @@ public class Q44_CheckTreeIsBalanced {
 		root.left.left.left = new TreeNode(5);
 		root.left.left.left.right = new TreeNode(6);
 		
-		System.out.println(treeBal.isBalanced(root, 0));		
+		System.out.println(treeBal.isBalanced(root, 0));
 	}
 	
 	private static void linkedList(TreeBalancer treeBal) {
 		TreeNode root = new TreeNode(1);
 		root.left = new TreeNode(2);
-		root.left.left = new TreeNode(4);
-		root.left.left.left = new TreeNode(5);
-		root.left.left.left.left = new TreeNode(6);
-		
-		System.out.println(treeBal.isBalanced(root, 0));		
+		root.left.left = new TreeNode(20);
+		root.left.left.left = new TreeNode(3);
+		root.left.left.left.left = new TreeNode(30);
+
+		//Tree.printBinaryTree(root);
+		System.out.println(treeBal.isBalanced(root, 0));
 	}
 }
 
 class TreeBalancer{
 	
-	boolean isBalanced = true;
+	boolean isBalanced;
+	
+	//My method is effectively similar to this
+	public int checkHeightCTCI(TreeNode root) {
+		if (root == null) {
+			return -1;
+		}
+		int leftHeight = checkHeightCTCI(root.left);
+		if (leftHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int rightHeight = checkHeightCTCI(root.right);
+		if (rightHeight == Integer.MIN_VALUE) return Integer.MIN_VALUE; // Propagate error up
+		
+		int heightDiff = leftHeight - rightHeight;
+		if (Math.abs(heightDiff) > 1) {
+			return Integer.MIN_VALUE; // Found error -> pass it back
+		} else {
+			return Math.max(leftHeight, rightHeight) + 1;
+		}
+	}
 	
 	boolean isBalanced(TreeNode root, int height){
-		
+		isBalanced = true;
 		@SuppressWarnings("unused")
 		int getHeight = getHeight(root, height);
 		return isBalanced;
 	}
 	
 	int getHeight(TreeNode root, int height){
+		
 		if(root == null || (root.left == null && root.right == null))
 			return 0;
 		
