6826dc9366a07888d62a9abd8ca377d7e364c1a0
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-09 18:48:00 -0400

    Added comments for merge k arrays

--- src/fb/se/array/MergeMultipleSortedArrays.java
+++ src/fb/se/array/MergeMultipleSortedArrays.java
@@ -8,6 +8,11 @@ import java.util.ArrayList;
 
 // This question is deceptively difficult!!!!!
 
+//TODO
+//The real solution is to use a min heap. So that way my solution-
+// kn * k 				becomes
+// kn * log (k)		<-- faster because of the logarithm time it takes for comparison instead of linear time.
+
 public class MergeMultipleSortedArrays {
 	
 	int[][] arrays;
@@ -24,8 +29,7 @@ public class MergeMultipleSortedArrays {
 	}
 	
 	public int[] merge(){
-		int n = 0;		
-		
+		int n = 0;				
 		
 		while(n<15){			//O(n)
 			int copyElementFromArray = findSmallest(arrays); //O(k)
