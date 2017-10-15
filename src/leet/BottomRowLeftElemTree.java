82e47b8143d4c87dbee1c5f2e83b8738c102a6e0
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-15 19:14:32 -0400

    Print leftmost of bottom row in tree LC_ 513_M

---
+++ src/leet/BottomRowLeftElemTree.java
@@ -0,0 +1,75 @@
+package leet;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BottomRowLeftElemTree {
+
+	 
+	 
+     public static int findBottomLeftValue(TreeNode root) {
+        if(root == null) return Integer.MIN_VALUE;
+        if(root.left == null) return root.val;
+		
+        Queue<TreeNode> children = new LinkedList<TreeNode>();
+        children.add(root);
+        int first = 0;
+        
+        while(!children.isEmpty()){
+        	first = children.peek().val;
+        	Queue<TreeNode> parents = children;
+        	children = new LinkedList<TreeNode>();
+        	
+        	for(TreeNode parent: parents){
+        		if(parent.left != null)
+        			children.add(parent.left);
+        		if(parent.right != null)
+        			children.add(parent.right);
+        	}
+        }        
+        return first;
+     }
+     
+     public static void main(String args[]){
+    	 singleElement();
+    	 exampleOneFromLeet();
+    	 exampleTwoFromLeet();
+     }
+
+	private static void exampleTwoFromLeet() {
+		TreeNode root = new TreeNode(1);
+		System.out.println(findBottomLeftValue(root));
+	}
+
+	private static void exampleOneFromLeet() {
+//	        1
+//	       / \
+//	      2   3
+//	     /   / \
+//	    4   5   6
+//	       /
+//	      7
+	      TreeNode root = new TreeNode(1);
+	      root.left = new TreeNode(2);
+	      root.left.left = new TreeNode(4);
+	      root.right = new TreeNode(3);
+	      root.right.left = new TreeNode(5);
+	      root.right.right = new TreeNode(6);
+	      root.right.left.left = new TreeNode(7);
+	      
+	      System.out.println(findBottomLeftValue(root));
+	}
+
+	private static void singleElement() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
