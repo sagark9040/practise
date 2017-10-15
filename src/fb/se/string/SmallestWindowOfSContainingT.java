803104f8c96ffcd78a5669d7c117f3e1ec7ee751
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-14 20:29:14 -0400

    Min window efficient method - linear time

--- src/fb/se/string/SmallestWindowOfSContainingT.java
+++ src/fb/se/string/SmallestWindowOfSContainingT.java
@@ -11,7 +11,7 @@ package fb.se.string;
 public class SmallestWindowOfSContainingT {
 
 	//////////////////////////////////////////////////////////////////////////////////
-	//							BruteForce Solution
+	//							BruteForce Solution O(N^2)
 	//////////////////////////////////////////////////////////////////////////////////
 	void smallestWindow(String main, String pat){
 		
@@ -30,7 +30,7 @@ public class SmallestWindowOfSContainingT {
 		int[] patChars = new int[256];
 		int count = pat.length();
 		
-		patChars = getPatChars(pat);
+		patChars = getCharacterCountArray(pat);
 		
 		for(int i=0; i<subString.length(); i++){
 			if(count>0){
@@ -46,23 +46,73 @@ public class SmallestWindowOfSContainingT {
 		return Integer.MAX_VALUE;
 	}
 	
-	private int[] getPatChars(String pat) {
-		int[] patChars = new int[256];
+	private int[] getCharacterCountArray(String str) {
+		int[] charCountArray = new int[256];
 		
-		for(Character c: pat.toCharArray()){
-			patChars[c]++;
+		for(Character c: str.toCharArray()){
+			charCountArray[c]++;
 		}
 
-		return patChars;
+		return charCountArray;
 	}
 	//////////////////////////////////////////////////////////////////////////////////
+	// 						Efficient Solution O(N)
+	//////////////////////////////////////////////////////////////////////////////////
+		
+	void smallestWindowLinearTime(String main, String pat){
+		int[] mainChars = new int[256];
+		int[] patChars = getCharacterCountArray(pat);
+		int count = 0, start = 0, startIndex = -1, minWindow = Integer.MAX_VALUE; 
+		int patLen = pat.length();		
+		
+		//sagargsagar
+		//rsg
+		
+		for(int i=0; i<main.length(); i++){
+			char c = main.charAt(i);
+			mainChars[c]++;
+
+			//C is present in pattern, increment count if we've not exceeded the value of C
+			if(patChars[c] != 0 &&
+					mainChars[c] <= patChars[c])
+				count++;
+				
+			// Found a window
+			if(count == patLen){
+				
+				char curChar = main.charAt(start);
+				
+				// extra characters or unnecessary characters
+				while(patChars[curChar]==0 || 
+						mainChars[curChar] > patChars[curChar]){	
+					
+					if(mainChars[curChar] > patChars[curChar])
+						mainChars[curChar]--;
+					
+					start++;
+					curChar = main.charAt(start);
+				}
+	
+				// update window size
+	            int len_window = i - start + 1;
+	            if (minWindow > len_window)
+	            {
+	                minWindow = len_window;
+	                startIndex = start;
+	            }
+			}			
+		}
+		System.out.println(minWindow);
+	}
 	
 	public static void main(String args[]){
-		String mainString = "sagarsagar";
+		String mainString = "sagargsagar";
 		String patternString = "rsg";
 		
 		SmallestWindowOfSContainingT smallestWindow = new SmallestWindowOfSContainingT();
-		smallestWindow.smallestWindow(mainString, patternString);
+		//smallestWindow.smallestWindow(mainString, patternString);
+		smallestWindow.smallestWindowLinearTime(mainString, patternString);
+		
 	}
 	
 }
