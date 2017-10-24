0e0037cd9945a1c2c7c567c375ab528de9044d21
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-24 15:04:50 -0400

    SysOut statements for clarity

--- src/fb/se/array/AllSubsetsOfArray.java
+++ src/fb/se/array/AllSubsetsOfArray.java
@@ -13,20 +13,28 @@ public class AllSubsetsOfArray {
 		System.out.println(1<<n);
         for (int i = 0; i < (1<<n); i++)
         { 
+        	System.out.println("Cur i:" + i + " -------------- " + Integer.toBinaryString(i));
         	List<Integer> list = new ArrayList<Integer>();
             for (int j = 0; j < n; j++){
  
+            	System.out.println(i + " & " + (1 << j)  + " --> "+(i & (1 << j)));
                 if ((i & (1 << j)) > 0)
                 	list.add(nums[j]);
             }
+
             
+            
+            System.out.print("---------------------------------------- [ ");
+            for(int l: list)
+            	System.out.print(l + " ");
+            System.out.println("] \n\n");
             allLists.add(list);
         }
 		return allLists;
 	}
 	public static void main(String args[]){
 		AllSubsetsOfArray all = new AllSubsetsOfArray();
-		int[] arr = {1,2,3};
-		all.subsets(arr);
+		int[] arr = {1,2,3, 4};
+		all.subsets(arr);				
 	}
 }
