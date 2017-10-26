ce8058b122354f49a59657877c17562195b9e1ac
Sagar Kangutkar <sagark9040@gmail.com>
2017-10-26 16:53:33 -0400

    Initial version of median of sorted array

--- src/leet/array/MedianOfSortedArrays.java
+++ src/leet/array/MedianOfSortedArrays.java
@@ -1,5 +1,13 @@
 package leet.array;
 
+
+
+////////////////////////////////////////
+////////
+////////		 INCOMPLETE
+////////
+////////////////////////////////////////
+
 public class MedianOfSortedArrays {
 	// Is this that hard? We can find/know the size of the two arrays.
     // Median will be the middle element, in case of odd sum of arrays.
@@ -14,11 +22,55 @@ public class MedianOfSortedArrays {
     // 
     
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-    
-        int len1 = nums1.length;
-        int len2 = nums2.length;
         
+        if(nums1.length == 0 && nums2.length == 0)
+        	return 0;
+        
+        int ptr1 = 0, ptr2 = 0;
+        boolean oddNumOfElements = false;
+        double stopperValA, stopperValB = 0;
+        
+        //sum is even
+        if((nums1.length + nums2.length) % 2 == 0){
+        	stopperValA = (nums1.length + nums2.length)/2 - 1;
+        	stopperValB = (nums1.length + nums2.length)/2;
+        }else{
+        	stopperValA = (int) (nums1.length + nums2.length)/2;
+        	stopperValB = stopperValA;
+        	oddNumOfElements = true;
+        }
+        
+        while(ptr1 < nums1.length && ptr2 < nums2.length){
+    		if(oddNumOfElements && ptr1+ptr2 == stopperValA-1){
+    			//display the next element
+    			return nums1[ptr1] > nums2[ptr2] ? nums2[ptr2] : nums1[ptr1];
+    		}else if(!oddNumOfElements && ptr1+ptr2 == stopperValB){
+    			return ((double)nums1[ptr1] + nums2[ptr2]) / 2;
+    		}
+    		
+    		if(nums1[ptr1] > nums2[ptr2])
+    			ptr2++;
+    		else
+    			ptr1++;
+        }
+        
+        // Handle the situation in nums2
+        if(ptr1 == nums1.length){
+        	
+        }
+
+        // Handle the situation in nums1
+        if(ptr2 == nums2.length){
+        	
+        }
+
 		return 0.0;
         
+    }
+    
+    public static void main(String args[]){
+    	MedianOfSortedArrays med = new MedianOfSortedArrays();
+    	int[] nums1 = {1, 3}, nums2 = {2, 4};
+    	System.out.println(med.findMedianSortedArrays(nums1, nums2));
     }
 }
