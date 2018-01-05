f38336e24e915a81accdc73a6f527ea0e9692620
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-04 22:54:39 -0500

    Add meat to LongestUnivaluePath function

---
+++ src/leet/misc/LongestUnivaluePath.java
@@ -0,0 +1,17 @@
+package leet.misc;
+
+public class LongestUnivaluePath {
+	public int longestUnivaluePath(TreeNode root) {
+		return 0;
+        
+    }
+	public static void main(String args[]){
+		
+	}
+}
+class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	  }
