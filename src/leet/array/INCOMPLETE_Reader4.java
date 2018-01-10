71b0e8271bebcfbb0ff29a72c57179a20792a7ce
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-09 22:20:25 -0500

    Pushing Reader4 question. Didn't understand it.

---
+++ src/leet/array/INCOMPLETE_Reader4.java
@@ -0,0 +1,14 @@
+package leet.array;
+
+public class INCOMPLETE_Reader4 {
+
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    public int read(char[] buf, int n) {
+        return buf.length%4;
+        		
+    }
+}
