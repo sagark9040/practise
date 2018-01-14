68d4f748cbb86fe41c2054d1792a4c424d77287f
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-13 20:38:13 -0500

    Empty method for longest common prefix.

---
+++ src/leet/string/LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+package leet.string;
+
+public class LongestCommonPrefix {
+	public String longestCommonPrefix(String[] strs) {
+    // Write a function to find the longest common prefix string amongst an array of strings.
+
+
+		
+		return "";
+    }
+}
