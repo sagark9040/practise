3e54e57617187c069c416949a0f193b4b9df4b63
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-15 19:19:44 -0500

    Constant space linear time algorithm for first missing positive

--- src/leet/array/FirstMissingPositive.java
+++ src/leet/array/FirstMissingPositive.java
@@ -4,6 +4,11 @@ public class FirstMissingPositive {
 	
 	// Thoughts & TCs:
 	// Repeated numbers.
+	// Something to do with sum? Smallest Positive and largest positive
+	// [-4, 1, 3, 4, 3, -100, 4, 0] --> 1+3+3+4+4+5 = 20 first missing positive = 2 
+	// Binary:
+	// 1 10 11 110 --> 5 101
+	
 	
 	// Approaches
 	
@@ -37,10 +42,43 @@ public class FirstMissingPositive {
 		
 		return largest+1;        
     }
+	
+	// CONSTANT SPACE BUT NOT REPEATING NUMBERS
+	
+	public int firstMissingPositive (int[] nums) {
+		if(nums == null || nums.length == 0)
+			return 1;
+		
+		int i = 0;
+		for (int num: nums) {			
+			
+			if(num > 0 && i != num-1 && num < nums.length) {
+				nums = swap(nums, num, i); 
+				
+			}
+			
+			i++;
+		}
+		
+		for (i=0; i<nums.length; i++) {
+			if(nums[i] != i+1)
+				return i+1;
+		}
+		
+		return nums[nums.length] + 1;
+	}
+	
+	private int[] swap(int[] nums, int num, int pos) {
+		int temp = nums[num-1]; 
+		nums[num-1] = num;
+		nums[pos] = temp;
+		return nums;
+	}
+
 	public static void main (String args[]) {
 		
 		int[] arr = {2}; // {3,4,-1,1};
 		FirstMissingPositive first = new FirstMissingPositive();
-		System.out.println(first.firstMissingPositiveLinearSpace(arr));
+		System.out.println(first.firstMissingPositive(arr));
 	}
 }
