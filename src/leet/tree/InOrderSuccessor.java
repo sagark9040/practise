430d361c8a4a8431ec53b788f47ab464a27ec465
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-16 16:22:18 -0500

    Created base for in order successor node

---
+++ src/leet/tree/InOrderSuccessor.java
@@ -0,0 +1,38 @@
+package leet.tree;
+
+/**
+Inorder Successor in BST
+Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
+
+Note: If the given node has no in-order successor in the tree, return null.
+
+Java	
+
+
+1
+
+2
+ * Definition for a binary tree node.
+3
+ * public class TreeNode {
+4
+ *     int val;
+5
+ *     TreeNode left;
+6
+ *     TreeNode right;
+7
+ *     TreeNode(int x) { val = x; }
+8
+ * }
+9
+ */
+
+class InOrderSuccessor {
+
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+		return p;
+
+    }
+
+}
