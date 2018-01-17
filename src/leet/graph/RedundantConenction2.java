68fad7ad39f82f3942b80ed28cfca14713e4ae19
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-17 17:44:04 -0500

    Example in comment

--- src/leet/graph/RedundantConenction2.java
+++ src/leet/graph/RedundantConenction2.java
@@ -3,3 +3,26 @@ package leet.graph;
 public class RedundantConenction2 {
 
 }
+
+/*
+
+
+Example 1:
+Input: [[1,2], [1,3], [2,3]]
+Output: [2,3]
+Explanation: The given directed graph will be like this:
+  1
+ / \
+v   v
+2-->3
+
+Example 2:
+Input: [[1,2], [2,3], [3,4], [4,1], [1,5]]
+Output: [4,1]
+Explanation: The given directed graph will be like this:
+5 <- 1 -> 2
+     ^    |
+     |    v
+     4 <- 3
+
+*/
