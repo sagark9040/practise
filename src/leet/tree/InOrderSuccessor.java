a09a4c8e0c8b9c4bf4465df3e0873008eb9651d5
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-16 20:09:07 -0500

    additions to in order succ

--- src/leet/tree/InOrderSuccessor.java
+++ src/leet/tree/InOrderSuccessor.java
@@ -1,5 +1,8 @@
 package leet.tree;
 
+import ctci.Chp4_TreesAndGraphs.Tree;
+import ctci.Chp4_TreesAndGraphs.TreeNode;
+
 /**
 Inorder Successor in BST
 Given a binary search tree and a node in it, find the in-order successor of that node in the BST.
@@ -28,11 +31,52 @@ Java
 9
  */
 
-class InOrderSuccessor {
+//Thoughts 
+// Iterate in order fashion and then once node p has been found, the next to show is the  successor
+
+//TCs
+//Empty tree
+//One element tree
+//Last leaf as p
+//Last of left subtree as p
+//Root as p
+//p not present
+//LinkedList as tree, p somewhere there
+
+public class InOrderSuccessor {
 
     public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
 		return p;
 
     }
+    
+    public static void main(String args[]) {
+    	TreeNode treeNode = new TreeNode(4);
 
+    	treeNode.left = new TreeNode(2);
+    	treeNode.left.left = new TreeNode(1);
+    	treeNode.left.right = new TreeNode(3);
+    	
+    	treeNode.right = new TreeNode(6);
+    	treeNode.right.left = new TreeNode(5);
+    	treeNode.right.right = new TreeNode(7);
+    	treeNode.right.right.right = new TreeNode(8);
+    	
+    	Tree tree = new Tree();
+    	printBinaryTree(treeNode, 0);
+    }
+    
+    public static void printBinaryTree(TreeNode root, int level){		
+	    if(root==null)
+	         return;
+	    printBinaryTree(root.right, level+1);
+	    if(level!=0){
+	        for(int i=0;i<level-1;i++)
+	            System.out.print("|\t");
+	            System.out.println("|-------"+root.data);
+	    }
+	    else
+	        System.out.println(root.data);
+	    printBinaryTree(root.left, level+1);
+	}   
 }
