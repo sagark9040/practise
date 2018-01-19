ea759ea955554b0dbc0f10be451aeb27d7a89739
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-18 23:16:23 -0500

    Added my own comment/suggestion on this. Removed examples.

--- src/leet/array/LC48M_RotateImageRotate2DArrayMatrix.java
+++ src/leet/array/LC48M_RotateImageRotate2DArrayMatrix.java
@@ -2,40 +2,12 @@ package leet.array;
 
 public class LC48M_RotateImageRotate2DArrayMatrix {
 
-	// You are given an n x n 2D matrix representing an image.
 	/*
-	Given input matrix = 
-			[
-			  [1,2,3],
-			  [4,5,6],
-			  [7,8,9]
-			],
 
-			rotate the input matrix in-place such that it becomes:
-			[
-			  [7,4,1],
-			  [8,5,2],
-			  [9,6,3]
-			]
-			
-			
-			
-			
-			Given input matrix =
-[
-  [ 5, 1, 9,11],
-  [ 2, 4, 8,10],
-  [13, 3, 6, 7],
-  [15,14,12,16]
-], 
+	---------------			IMPORTANT          --------------- 
 
-rotate the input matrix in-place such that it becomes:
-[
-  [15,13, 2, 5],
-  [14, 3, 4, 1],
-  [12, 6, 8, 9],
-  [16, 7,10,11]
-]
+	REMEMBER THIS NEAT AND CLEAN	  <<<  TRANSPOSE-BASED SOLUTION  >>>       FOR THE REST OF YOUR LIFE !!!!!!! 
+
 	*/
 	
 	public void rotate(int[][] matrix) {
