268bfec2a7b07503dfe3f7824a98c4e1d6bf104a
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-26 23:59:03 -0500

    Both solutions for K largest .. constant space soln and constant time solutions

---
+++ src/leet/array/FindKthLargestElementInArray.java
@@ -0,0 +1,38 @@
+package leet.array;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class FindKthLargestElementInArray {
+	
+	// DRY RUN KAR RE BABA!!!
+	
+	   public int findKthLargest(int[] nums, int k) {
+	        PriorityQueue<Integer> queue = new PriorityQueue<>(k, Collections.reverseOrder());
+	
+	        if(nums == null || nums.length == 0)
+	            return 0;
+	
+	        for(int i=0; i<nums.length; i++)
+	            queue.offer(nums[i]);
+	        
+	        int v = 0;						// BECAME LAZY AND DIDN'T DO DRY RUN HERE!!
+	        while(k > 0) {
+	            v = queue.poll(); k--;
+	        }
+	        return v;
+	    }
+
+	    
+	    
+	// N log N solution in place
+	// --------------------------    
+//	     public int findKthLargest(int[] nums, int k) {
+//	         if(nums == null || nums.length == 0)
+//	             return 0;
+//	         Arrays.sort(nums); // N log N solution
+//	         return nums[nums.length - k];
+
+//	     }
+	
+}
