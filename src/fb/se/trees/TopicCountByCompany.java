171d3d82d5d392fba943c96f085fb47119d0dd12
Sagar Kangutkar <sagark9040@gmail.com>
2018-01-27 19:58:36 -0500

    Adding topic count by companies as per LC

---
+++ src/fb/se/trees/TopicCountByCompany.java
@@ -0,0 +1,57 @@
+package fb.se.trees;
+
+public class TopicCountByCompany {
+	// LinkedIn
+	
+	//Tree 22
+	//Array 16
+	//Hash 8
+	//DP 8
+	//String 8
+	//DFS 7
+	//BFS 5
+	//Stack 4
+	//Backtracking 3
+	//LinkedList 2
+	//Heap 1
+	//Bit manip 1
+	//Queue NIL
+	
+	// Amazon -- 31/85
+	
+	//Array 22
+	//Tree 32
+	//String 23
+	//Hash 17
+	//Linked List 14
+	//Two Pointers 8
+	//BFS 7
+	//DP 6
+	//Heap 6
+	//Stack 5
+	//Greedy 2
+	//DFS 6
+	//Backtracking 4
+	//Bit manip 2
+	//Queue NONE
+
+	// Google
+	
+	//Array 48
+	//String 48
+	//Tree 47
+	//DP 31
+	//Hash Table 27
+	//DFS 22
+	//Stack 15
+	//Graph 14
+	//BFS 13
+	//Linked List 5
+	//Backtracking 14
+	//Heap 13
+	//Bit Manip 10
+	//Divide and Conquer 10
+	//Greedy 12
+
+	//Queue 4
+}
