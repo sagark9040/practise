4db887d6f83287e0bc78ea776182c13fa8fafa41
Sagar Kangutkar <sagark9040@gmail.com>
2018-02-25 18:08:58 -0500

    maxArea .. array .. decreasing window from 0 to n-1 inwards

---
+++ src/leet/array/LC11M_MaxArea.java
@@ -0,0 +1,32 @@
+package leet.array;
+
+public class LC11M_MaxArea {
+
+	//406  
+    // did it like the wall-trapping rainwater one in 20mins ..
+    // saw the soln and doing it for lines 
+    //error on right-left multiplier
+
+    public int maxArea(int[] height) {
+        if(height.length == 0 || height.length == 1) {
+            return 0;
+        }
+        
+        int left = 0, right = height.length-1;
+        int maxArea = 0;
+        while(left < right) {
+            int curArea = Math.min(height[left], height[right]) * (right-left);
+            maxArea = Math.max(maxArea, curArea);            
+            
+            if(height[left] < height[right]) left++;
+            else right--;
+        }
+        return maxArea;
+    }
+    
+	public static void main(String args[]) {
+		int[] height = {1,1};
+		LC11M_MaxArea ma = new LC11M_MaxArea();
+		System.out.println(ma.maxArea(height));
+	}
+}
