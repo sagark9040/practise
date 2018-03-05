da21e2172ae62b14cb775c726251f2b9068bc207
Sagar Kangutkar <sagark9040@gmail.com>
2018-03-05 12:52:05 -0500

    easy..filler

---
+++ src/leet/tree/LC100E_IsSameTree.java
@@ -0,0 +1,10 @@
+package leet.tree;
+
+public class LC100E_IsSameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null)  return true;
+        if(p == null || q == null)  return false;
+        if(p.val != q.val)  return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
