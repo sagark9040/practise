a79738b3727f49b1f5d796ed7cb16a1300dceabb
Sagar Kangutkar <sagark9040@gmail.com>
2018-04-19 15:16:43 -0400

    code

--- src/leet/math/LC168E_ExcelSheetColumnTitle.java
+++ src/leet/math/LC168E_ExcelSheetColumnTitle.java
@@ -1,5 +1,19 @@
 package leet.math;
 
 public class LC168E_ExcelSheetColumnTitle {
-
+	// took more time than I think necessary
+	
+	public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        char[] charVal = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        
+        while(n > 0) {
+            n--;
+            sb.append(charVal[n%26]);
+            n /= 26;            
+        }
+        
+        return sb.reverse().toString();
+    }
 }
